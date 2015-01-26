53bec6e11184e66982191513b242546645d54dad
Nandu Kalidindi <nandukalidindi@gmail.com>
2015-01-26 23:20:11 +0530

    Transitioning to show Only Complete Todos
    Similar to active route, a new completed route is created

--- js/router.js
+++ js/router.js
@@ -1,7 +1,20 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function() {
     this.route('active');
+    this.route('completed');
   });
+});
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('todo');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
 });
 
 Todos.TodosActiveRoute = Ember.Route.extend({
@@ -15,14 +28,13 @@ Todos.TodosActiveRoute = Ember.Route.extend({
   }
 });
 
-Todos.TodosIndexRoute = Ember.Route.extend({
+Todos.TodosCompletedRoute = Ember.Route.extend({
   model: function() {
-    return this.modelFor('todos');
-  }
-});
-
-Todos.TodosRoute = Ember.Route.extend({
-  model: function () {
-    return this.store.find('todo');
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
