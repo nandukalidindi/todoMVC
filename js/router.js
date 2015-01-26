e138f571d1669170283842206c61981badbe44a5
Nandu Kalidindi <nandukalidindi@gmail.com>
2015-01-26 22:48:34 +0530

    Adding Child Routes
    Outlet is an area which gets rendered dynamically as we transition between routes. More Understanding required

--- js/router.js
+++ js/router.js
@@ -1,5 +1,13 @@
 Todos.Router.map(function () {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+
+  });
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
 });
 
 Todos.TodosRoute = Ember.Route.extend({
