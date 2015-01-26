865dc34feb4cdad14ff7cae0d491b91e09c203d7
Nandu Kalidindi <nandukalidindi@gmail.com>
2015-01-26 23:13:33 +0530

    Transitioning to Show Only Incomplete Todos
    The controller in the renderTemplate function generates a new todo:active controller but since the each loops over on the todo Item controller it wont affect anything. And the outlet is filled as per the template mentioned in the renderTemplate otherwise it doesnt know which to fill in it with.

--- js/router.js
+++ js/router.js
@@ -1,7 +1,18 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function() {
-
+    this.route('active');
   });
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
 });
 
 Todos.TodosIndexRoute = Ember.Route.extend({
